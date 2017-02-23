49f555c1f7f23d4d3e6ecd34f17a2555ec3874f6
KibaNaoParsa <elyasnog2@hotmail.com>
2017-02-23 11:44:42 -0300

    Exercício de Carnaval

--- Ponto.java
+++ Ponto.java
@@ -33,14 +33,9 @@ public class Ponto {
 		Ponto p2 = new Ponto();
 		p2.setX(30);
 		p2.setY(10);
-		Reta r1 = new Reta();
+		Reta r1 = new Reta();		
 		r1.setP1(p1);
-		r1.setP2(p2);
-		double baseme = r1.getP2().getY();
-		double basema = r1.getP1().getY();
-		double height = r1.getP2().getX() - r1.getP1().getX();
-		double area = ((baseme + basema) * height)/2;
-		area = Math.abs(area);
-		System.out.println(area); 
+		r1.setP2(p2); 
+		System.out.println(r1.Area());
 	}
 }
--- Reta.java
+++ Reta.java
@@ -16,4 +16,14 @@ public class Reta {
 	public Ponto getP2() {
 		return this.p2;
 	}
+
+	public double Area() {
+		
+		double baseme = this.getP2().getY();
+		double basema = this.getP1().getY();
+		double height = this.getP2().getX() - this.getP1().getX();
+		double area = ((baseme + basema) * height)/2;
+		area = Math.abs(area);
+		return area;
+	}
 }
